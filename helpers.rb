b90f39b6d9fa26d4d722100463fcbc0984298acd
Maimer <nesnl23@yahoo.com>
2014-05-22 13:52:23 -0400

    Added submit page and url/blank verifications

--- helpers.rb
+++ helpers.rb
@@ -6,3 +6,16 @@ def make_data()
   articles
 end
 
+def check_blanks(author, title, url, desc)
+  if author == "" || title == "" || url == "" desc == ""
+    return true
+  end
+  false
+end
+
+def check_url(url)
+  if Net::HTTP.get_response(URI.parse(url)).code != "200"
+    return true
+  end
+  false
+end
--- server.rb
+++ server.rb
@@ -7,3 +7,23 @@ get '/' do
   @articles = make_data().reverse
   erb :index
 end
+
+get '/submit' do
+  @author = ""
+  @title = ""
+  @url = ""
+  @desc = ""
+  erb :'submit/submit'
+end
+
+post '/submit' do
+  @author = params["author"]
+  @title = params["title"]
+  @url = params["url"]
+  @desc = params["desc"]
+  if check_blanks(@author, @title, @url, @desc) && check_url(@url) && check_desc(@desc)
+    erb :'submit/submit'
+  else
+    save_post(@author, @title, @url, @desc)
+  end
+end
