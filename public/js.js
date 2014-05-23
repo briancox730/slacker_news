7863d6ab44ee1b65c83e4f6ac8a3c4edcd56ddf8
Brian Cox <briancox730@gmail.com>
2014-05-23 11:13:39 -0400

    Complete JS validation

--- public/js.js
+++ public/js.js
@@ -1,6 +1,32 @@
 function validateForm() {
+  var result = true;
   if (document.submit.author.value == "") {
     document.getElementById("author_alert").innerHTML = "*Required";
-    return false;
-  }
+    result = false;
+  } else {
+    document.getElementById("author_alert").innerHTML = "";
+    result = true;
+  };
+  if (document.submit.title.value == "") {
+    document.getElementById("title_alert").innerHTML = "*Required";
+    result = false;
+  } else {
+    document.getElementById("title_alert").innerHTML = "";
+    result = true;
+  };
+  if (document.submit.url.value == "") {
+    document.getElementById("url_alert").innerHTML = "*Required";
+    result = false;
+  } else {
+    document.getElementById("url_alert").innerHTML = "";
+    result = true;
+  };
+  if (document.submit.desc.value == "" || document.submit.desc.value.length < 20) {
+    document.getElementById("desc_alert").innerHTML = "*Must be more than 20 characters";
+    result = false;
+  } else {
+    document.getElementById("desc_alert").innerHTML = "";
+    result = true;
+  };
+  return result
 }
